8474ac47905bb2f7bfd48c052a2bb73019253cc6
junho <junho.ko@business-canvas.com>
2023-04-30 12:59:57 +0900

    refactor: wrapped with memoization memoUsecase

--- src/client/memo/hooks/useMemoUsecase.ts
+++ src/client/memo/hooks/useMemoUsecase.ts
@@ -1,17 +1,21 @@
 import { Memo } from "@/types/memo";
 import { MemoUsecase } from "../usecase";
 import { Store } from "@/server/store/localStorage/store";
+import { useCallback, useMemo } from "react";
 
 export const useMemoUsecase = (): MemoUsecase => {
-  const store = new Store();
+  const store = useMemo(() => new Store(), []);
 
-  const getMemoList = (): Promise<Memo[]> => {
+  const getMemoList = useCallback((): Promise<Memo[]> => {
     return store.getMemoList();
-  };
+  }, [store]);
 
-  const saveMemo = (memo: Pick<Memo, "title" | "content">): Promise<Memo> => {
-    return store.createMemo(memo);
-  };
+  const saveMemo = useCallback(
+    (memo: Pick<Memo, "title" | "content">): Promise<Memo> => {
+      return store.createMemo(memo);
+    },
+    [store]
+  );
 
   return {
     getMemoList,
