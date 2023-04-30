dab69a9fffb8e63be22bddee798f93874a9dd1b5
junho <junho.ko@business-canvas.com>
2023-04-30 14:03:24 +0900

    feat: add updateMemo in saveMemo

--- src/client/memo/hooks/useMemoUsecase.ts
+++ src/client/memo/hooks/useMemoUsecase.ts
@@ -11,7 +11,15 @@ export const useMemoUsecase = (): MemoUsecase => {
   }, [store]);
 
   const saveMemo = useCallback(
-    (memo: Pick<Memo, "title" | "content">): Promise<Memo> => {
+    (
+      memo:
+        | Pick<Memo, "title" | "content">
+        | Pick<Memo, "id" | "title" | "content">
+    ): Promise<Memo> => {
+      if ("id" in memo) {
+        return store.updateMemo(memo);
+      }
+
       return store.createMemo(memo);
     },
     [store]
